4f9a57b13aef51131d4e15e90e078e59c0a5dce9
alexandra-kruip <hauser.alex1@gmail.com>
2017-05-02 11:48:53 -0700

    dynamic board created

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-/**
- * Created by alexhauser on 5/2/17.
- */
+$(document).ready(function() {
+   for(var x = 0; x<=5; x++){
+       for( var y = 0; y <= 6 ; y++) {
+           var div = $('<div>').text(x + " , " + y);
+           $('body').append(div);
+
+       }
+   }
+
+});
