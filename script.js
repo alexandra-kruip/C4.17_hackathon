d474a69b4317703465e2418c918f60a6a8bc479e
Jackrayallday <jackray3111@gmail.com>
2017-05-03 09:16:20 -0700

    HTML Skel

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 var array = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
-]; // x = big array
+    ['','','','','','',''],     //nested arrays with cells for our columns
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','',''],
+    ['','','','','','','']
+
+]; // array = outer array of columns
+
 
 $(document).ready(function() {
     createBoard();
@@ -14,26 +16,34 @@ $(document).ready(function() {
 });
 
 
-function createBoard () {
-
+function createBoard () {    //dynamically creates our game board
     for(var x = 0; x <= 6 ; x++){
         var outerDiv = $('<div>').addClass('col' + x).addClass('col').attr('keyValue', x);
         for(var y = 0; y<=5; y++){
-            var innerDiv = $('<div>').addClass("innerDiv");
-                // .text(x + " , " + y).addClass('col' + y);
+            var innerDiv = $('<div>').addClass("innerDiv").attr('cell', y);
             outerDiv.append(innerDiv);
         }
-        $('body').append(outerDiv);
+        $('#gameArea').append(outerDiv);
     }
 }
 // function selectingCol(){
 // }
 function colClicked () {
-    var clickedCol = $(this).attr("keyValue");
-    array[clickedCol].push("1");
+    var clicked = $(this).attr("keyValue");
+    var cell = $(this).children().attr("cell");
 
-    console.log(array);
+    $(".innerDiv").addClass('player1');
 
+    for (var i = 0; i < array[clicked].length; i++){  // loops through array to see where to place coin
+        if(array[clicked][i] === ''){
+            array[clicked][i] = "1";
+            return;
+        }
+        console.log(array[clicked]);
+    }
+
+
+    // switch players
 
 }
 
@@ -58,4 +68,3 @@ function colClicked () {
 // take last available spot (y = 5)
 //
 // $(openSpot).addClass(‘takenPlayer3’);
-// }
